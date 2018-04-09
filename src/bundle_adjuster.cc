74c4977939654881cd8cd41ed87f6daa19e0c76c
tuckerhaydon <tuckerhaydon@gmail.com>
2018-04-09 14:23:20 -0500

    Updated atan loss

--- src/bundle_adjuster.cc
+++ src/bundle_adjuster.cc
@@ -23,7 +23,7 @@ void BundleAdjuster::Solve() {
                 );
     
             problem.AddResidualBlock(cost_function,
-                                     new ceres::ArctanLoss(5),
+                                     new ceres::ArctanLoss(1),
                                      cam->QVec().data(),
                                      cam->TVec().data());
         }
@@ -37,7 +37,7 @@ void BundleAdjuster::Solve() {
                 );  
 
             problem.AddResidualBlock(cost_function,
-                                     new ceres::ArctanLoss(5),
+                                     new ceres::ArctanLoss(1),
                                      cam->QVec().data(),
                                      cam->TVec().data(),
                                      observation.GetFeature()->Pos().data()
